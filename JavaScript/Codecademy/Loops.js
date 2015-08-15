8fe17eff13855415ad4dad1314ba74e13a736335
glennvdl <glenn.vdl@gmail.com>
2015-08-16 00:16:39 +0200

    Loops added

---
+++ JavaScript/Codecademy/Loops.js
@@ -0,0 +1,13 @@
+//# For loop introduction
+
+//Example 1
+
+for (var counter = 1; counter < 11; counter++) {
+	console.log(counter);
+}
+
+//Example 2
+
+for (var i = 5; i < 11; i = i + 1){
+	console.log(i);
+}
